0558d056391003736c09cd134007dfddcd2c1312
kaminyou ho <kaminyouho@aetherai.com>
2022-02-24 02:35:14 +0800

    connect backend & ending log

--- frontend/src/containers/QualityTask.js
+++ frontend/src/containers/QualityTask.js
@@ -1,6 +1,7 @@
 import Button from '@material-ui/core/Button';
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import swal from "sweetalert";
 import ImageWrapper from '../components/ImageWrapper';
 import TransferList from "../components/TransferList";
 import configData from "../config.json";
@@ -11,61 +12,76 @@ function QualityTask({ account, task }) {
 	const rootImageURL = `${configData.SERVER_URL}/api/v1/images/`
 	const [challenges, setChallenges] = useState([]);
 	const [flag, setFlag] = useState(false);
+	const [currIdx, setCurrIdx] = useState(0);
+	const [candidate, setCandidate] = useState(['A', 'B', 'C']);
+	const [result, setResult] = useState([]);
 
 	
 	const getQualityChallenges = () => {
 		
 		axios.get(`${configData.SERVER_URL}/api/v1/quality_challenges`)
 		.then((res) => { 
-            var challenges_list = []
-			for (var id in res.data.data) {
+            let challenges_list = []
+			for (let id in res.data.data) {
 				const item = Object.assign({"ID": id}, res.data.data[id]);
 				challenges_list.push(item);
 			}
 			setChallenges(challenges_list);
 			setFlag(true);
-			})
+		})
         .catch((error) => { 
             setFlag(false);
             console.error(error);
-            }
-        );
+        });
 	};
 
 	useEffect(() => {
 		getQualityChallenges();
 	},[]);
 
-	const [currIdx, setCurrIdx] = useState(0);
-	const [candidate, setCandidate] = useState(['A', 'B', 'C']);
-	const [result, setResult] = useState([]);
 
-	const verifySelection = () => {
+	const verifySelection = (result) => {
+		if (result.length !== 3) return false
 		return true
 	}
 	
+	const resetResult = () => {
+		setCandidate(['A', 'B', 'C']);
+		setResult([])
+	}
+
 	const handleButtonOnClick = () => {
-		// console.log(result);
-		/*
-		if (verifySelection()) {
+		console.log(result);
+		if (verifySelection(result)) {
 			axios.post('http://10.1.0.41:9292/api/v1/record',{
 				account: account,
 				task: task,
-				challengeID: '41232132',
-				choice: 'a'
+				challengeID: challenges[currIdx]["ID"],
+				choice: result
 			})
-			.then( (response) => console.log(response))
-			.catch( (error) => console.log(error))
+			.then( (response) => {
+				console.log(response)
+				resetResult();
+				setCurrIdx(currIdx + 1)
+			})
+			.catch( (error) => {
+				console.log(error)
+				swal({
+					title: "Error",
+					text: "Submission failure",
+					icon: "error",
+				});
+			})
+			
 		}
 		else {
 			swal({
 				title: "Error",
-				text: "Submission failure",
+				text: "Please rank all the candidates",
 				icon: "error",
 			});
 		}
-		*/
-		// setCurrIdx(currIdx + 1)
+		
 	}
 
 	const renderChallenge = () => {
@@ -110,9 +126,13 @@ function QualityTask({ account, task }) {
 
 	return (
 		<>
-			<p>Rank the candidates according to their qualites</p>
+			<p>{(flag && currIdx !== challenges.length) ? "[" + (currIdx + 1) + " / "+ challenges.length + "]" : ""} Rank the candidates according to their qualites</p>
 			{(flag) ? (
-					renderChallenge()
+					(currIdx === challenges.length) ? (
+						<p>Thank you for your participation</p>
+					) : (
+						renderChallenge()
+					)
 				) : (
 					<p>loading</p>
 			)}
